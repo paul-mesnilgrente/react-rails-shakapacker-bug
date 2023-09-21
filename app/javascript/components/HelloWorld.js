dacb74488c9449ebfdd86cb361c6376fe20bc376
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-09-21 22:49:03 +0200

    step 6

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,11 @@
 import React from "react"
-import PropTypes from "prop-types"
 
 const HelloWorld = (props) => {
   return (
-    <React.Fragment>
-      Greeting: {props.greeting}
-    </React.Fragment>
+    <p>
+      Hello {props.name}!
+    </p>
   )
 }
-
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
 
 export default HelloWorld
